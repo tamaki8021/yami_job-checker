0f6b501ab884bd848b4f11ef53e062d0d6a8a53c
tamaki8021 <hbk2002320@gmail.com>
2023-06-17 09:06:37 +0900

    modify: move file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '../reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
