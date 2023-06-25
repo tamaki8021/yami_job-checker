18987ee569f4e10b9817f5a5e825e73b3c76135d
tamaki8021 <hbk2002320@gmail.com>
2023-06-25 17:15:07 +0900

    🐛 fix: deploy

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'about',
-    element: <div>About</div>,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+    {
+      path: 'about',
+      element: <div>About</div>,
+    },
+  ],
+  { basename: process.env.PUBLIC_URL },
+);
 
 export default router;
