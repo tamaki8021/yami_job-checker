ff9f5757626e1f45554dec6c06bc5c1d4bd2943b
tamaki8021 <hbk2002320@gmail.com>
2023-06-28 00:31:13 +0900

    ✨ feat(ui): 診断テストページの土台を作成

---
+++ src/pages/Question/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from '@nextui-org/react';
+import Layout from '../../components/layout';
+import Box from '../../components/common/Box';
+
+const QuestionPage = () => (
+  <Layout>
+    <Box>
+      <Text>question</Text>
+    </Box>
+  </Layout>
+);
+
+export default QuestionPage;
