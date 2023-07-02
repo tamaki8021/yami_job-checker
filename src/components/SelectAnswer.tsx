a7deafc04f8f3ac6d585ac2796f290ded31a86f5
tamaki8021 <hbk2002320@gmail.com>
2023-07-02 23:54:30 +0900

    🐛 fix: buildが失敗したので修正

--- src/components/SelectAnswer.tsx
+++ src/components/SelectAnswer.tsx
@@ -8,38 +8,36 @@ type Props = {
   setValue: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const SelectAnswer: React.FC<Props> = ({ title, value, setValue }) => {
-  return (
-    <Box css={{ textAlign: 'center', w: '100%' }}>
-      <Text size="$lg" weight="bold">
-        {title}
-      </Text>
+const SelectAnswer: React.FC<Props> = ({ title, value, setValue }) => (
+  <Box css={{ textAlign: 'center', w: '100%' }}>
+    <Text size="$lg" weight="bold">
+      {title}
+    </Text>
 
-      <Radio.Group
-        orientation="horizontal"
-        value={value}
-        onChange={setValue}
-        css={{ alignItems: 'center', mt: '$5' }}
-      >
-        <Radio value="1" color="secondary" size="xl" />
-        <Radio value="2" color="secondary" size="lg" />
-        <Radio value="3" />
-        <Radio value="4" color="error" size="lg" />
-        <Radio value="5" color="error" size="xl" />
-      </Radio.Group>
-      <Box
-        css={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          maxW: '300px',
-          mx: 'auto',
-        }}
-      >
-        <Text color="secondary">同意する</Text>
-        <Text color="error">同意しない</Text>
-      </Box>
+    <Radio.Group
+      orientation="horizontal"
+      value={value}
+      onChange={setValue}
+      css={{ alignItems: 'center', mt: '$5' }}
+    >
+      <Radio value="1" color="secondary" size="xl" />
+      <Radio value="2" color="secondary" size="lg" />
+      <Radio value="3" />
+      <Radio value="4" color="error" size="lg" />
+      <Radio value="5" color="error" size="xl" />
+    </Radio.Group>
+    <Box
+      css={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        maxW: '300px',
+        mx: 'auto',
+      }}
+    >
+      <Text color="secondary">同意する</Text>
+      <Text color="error">同意しない</Text>
     </Box>
-  );
-};
+  </Box>
+);
 
 export default SelectAnswer;
