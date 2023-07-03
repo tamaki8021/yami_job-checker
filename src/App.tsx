1a9393de20baba0dbc89ae702ccb0d3803b8f0cf
tamaki8021 <hbk2002320@gmail.com>
2023-07-03 15:35:12 +0900

    🔧 chore: ga4の追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { RouterProvider } from 'react-router-dom';
 import { darkTheme, lightTheme } from './utils/theme';
 import router from './router';
 import { AnswerProvider } from './provider/AnswerProvider';
+import usePageTracking from './utils/hooks/useTracking';
 
 const App = () => {
   const darkMode = useDarkMode(false);
+  usePageTracking();
 
   return (
     <NextUIProvider theme={darkMode.value ? darkTheme : lightTheme}>
